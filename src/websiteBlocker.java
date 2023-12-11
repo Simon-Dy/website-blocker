8a6441e5f773ba3b98ce20a49384c58de36ee24d
Simon Dy <simon-julian.dyroff@informatik.hs-fulda.de>
2023-12-11 17:31:22 +0100

    add RegEx to check for valid websites, smarter saving without empty lines

--- src/websiteBlocker.java
+++ src/websiteBlocker.java
@@ -14,243 +14,274 @@ import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class websiteBlocker extends JFrame {
 
-    private JTextArea textArea;
-    
-    String filePath = "../website-blocker/hosts";
-    String filePathBackup = "../website-blocker/hosts_backup";
+	private JTextArea textArea;
+
+	String filePath = "../website-blocker/hosts";
+	String filePathBackup = "../website-blocker/hosts_backup";
 	String filePathSave = "../website-blocker/Blacklist.txt";
-	
-    public websiteBlocker() {
-        // Setzt die Grundkonfiguration für das JFrame
-        setTitle("Website-Blocker");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Erstellt eine JTextArea für den Text
-        textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
+	public websiteBlocker() {
+		// Setzt die Grundkonfiguration für das JFrame
+		setTitle("Website-Blocker");
+		setSize(420, 320);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton saveButton = new JButton("Block Website");
-        JButton manageButton = new JButton("Show blocked Websites");
-        JButton backButton = new JButton("Back");
-        backButton.setVisible(false);
+		// Erstellt eine JTextArea für den Text
+		textArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(textArea);
 
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                speichern();
-            }
-        });
+		JButton saveButton = new JButton("Block Website");
+		JButton manageButton = new JButton("Show blocked Websites");
+		JButton backButton = new JButton("Back");
+		backButton.setVisible(false);
 
-        manageButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	saveButton.setVisible(false);
-            	manageButton.setVisible(false);
-            	backButton.setVisible(true);
-            	textArea.setEditable(false);
-            	textArea.setText(getFile(filePathSave));
-            }
-        });
-        
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	saveButton.setVisible(true);
-            	manageButton.setVisible(true);
-            	backButton.setVisible(false);
-            	textArea.setEditable(true);
-            	textArea.setText("");
-            }
-        });
+		saveButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				speichern();
+			}
+		});
 
-        getContentPane().setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        
-        // JTextArea: nimmt mehrere Zellen im Gitter in Anspruch (ganze Spalte)
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2; // Zwei Zellen in der Breite
-        gbc.gridheight = 1; // Eine Zelle in der Höhe
-        gbc.fill = GridBagConstraints.BOTH; // Füllt sowohl horizontal als auch vertikal
-        gbc.weightx = 1.0; // Gewichtung in horizontaler Richtung
-        gbc.weighty = 8.0; // Gewichtung in vertikaler Richtung
-        getContentPane().add(scrollPane, gbc);
+		manageButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveButton.setVisible(false);
+				manageButton.setVisible(false);
+				backButton.setVisible(true);
+				textArea.setEditable(false);
+				textArea.setText(getFile(filePathSave));
+			}
+		});
 
-        // Clear Button: nimmt eine Zelle im Gitter ein
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 1; // Eine Zelle in der Breite
-        gbc.gridheight = 1; // Eine Zelle in der Höhe
-        gbc.fill = GridBagConstraints.BOTH; // Füllt horizontal
-        gbc.weightx = 1.0; // Keine Gewichtung in horizontaler Richtung
-        gbc.weighty = 4.0; // Keine Gewichtung in vertikaler Richtung
-        getContentPane().add(saveButton, gbc);
+		backButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveButton.setVisible(true);
+				manageButton.setVisible(true);
+				backButton.setVisible(false);
+				textArea.setEditable(true);
+				textArea.setText("");
+			}
+		});
 
-        // Additional Button: nimmt eine Zelle im Gitter ein
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 1; // Eine Zelle in der Breite
-        gbc.gridheight = 1; // Eine Zelle in der Höhe
-        gbc.fill = GridBagConstraints.BOTH; // Füllt horizontal
-        gbc.weightx = 1.0; // Keine Gewichtung in horizontaler Richtung
-        gbc.weighty = 1.0; // Keine Gewichtung in vertikaler Richtung
-        getContentPane().add(manageButton, gbc);
-        
-        // Additional Button: nimmt eine Zelle im Gitter ein
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.gridwidth = 1; // Eine Zelle in der Breite
-        gbc.gridheight = 1; // Eine Zelle in der Höhe
-        gbc.fill = GridBagConstraints.BOTH; // Füllt horizontal
-        gbc.weightx = 1.0; // Keine Gewichtung in horizontaler Richtung
-        gbc.weighty = 1.0; // Keine Gewichtung in vertikaler Richtung
-        getContentPane().add(backButton, gbc);
-        
-        checkForBackup();
-    }
-    
-    // Wenn der Benutzer auf "Speichern" klickt
-    private void speichern() {
-        // Zeigt einen Dialog zum Speichern an
-        //JFileChooser fileChooser = new JFileChooser();
-        //int result = fileChooser.showSaveDialog(this);
-         
-    	 boolean positionFound = false;
-    	 long position = 0;
-    	 String searchWord = "#Website-Blacklist";
-    	 
-         try {
-             // Öffnet die Datei zum Lesen
-        	 RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
+		getContentPane().setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
 
-             String line;
-             int lineNumber = 0;
+		// JTextArea: nimmt mehrere Zellen im Gitter in Anspruch (ganze Spalte)
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2; // Zwei Zellen in der Breite
+		gbc.gridheight = 1; // Eine Zelle in der Höhe
+		gbc.fill = GridBagConstraints.BOTH; // Füllt sowohl horizontal als auch vertikal
+		gbc.weightx = 1.0; // Gewichtung in horizontaler Richtung
+		gbc.weighty = 8.0; // Gewichtung in vertikaler Richtung
+		getContentPane().add(scrollPane, gbc);
 
-             // Durchsucht jede Zeile nach dem gesuchten Wort
-             while ((line = randomAccessFile.readLine()) != null) {
-                 lineNumber++;
+		// Clear Button: nimmt eine Zelle im Gitter ein
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 1; // Eine Zelle in der Breite
+		gbc.gridheight = 1; // Eine Zelle in der Höhe
+		gbc.fill = GridBagConstraints.BOTH; // Füllt horizontal
+		gbc.weightx = 1.0; // Keine Gewichtung in horizontaler Richtung
+		gbc.weighty = 4.0; // Keine Gewichtung in vertikaler Richtung
+		getContentPane().add(saveButton, gbc);
 
-                 // Überprüft, ob das gesuchte Wort in der Zeile enthalten ist
-                 if (line.contains(searchWord)) {
-                     //System.out.println("Das Wort '" + searchWord + "' wurde in Zeile " + lineNumber + " gefunden.");
-                     // Hier können Sie weitere Aktionen durchführen, z.B. die Position des Wortes in der Zeile anzeigen
-                     position = randomAccessFile.getFilePointer();
-                     positionFound = true;
-                     //System.out.println("Position des Wortes: " + position);
-                 }
-             }
-             // Schließt den BufferedReader
-             randomAccessFile.close();
-             
-             if (!positionFound) {
-            	 BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
-                 writer.write("\n" + searchWord);
-                 writer.newLine();
-                 writer.close();
-                 positionFound = true;
-             }
-         } catch (IOException e) {
-             e.printStackTrace();
-             System.err.println("Fehler beim Lesen der Datei.");
-         }
-        
-        if (filePath != null && positionFound) {
-        	if(position == 0) {
-        		try {
-	                // Erstellt eine BufferedWriter, um ans Ende der Datei zu schreiben
-	                BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
-	                writer.write("127.0.0.1 " + textArea.getText());
-	                writer.newLine();
-	                writer.close();
-	                
-	                putToSave(textArea.getText());
-	                JOptionPane.showMessageDialog(this, "Text erfolgreich gespeichert.");
-	                textArea.setText("");
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	                JOptionPane.showMessageDialog(this, "Fehler beim Speichern der Datei.");
-	            }
-        	} 
-        	else {
-        		try {
-                    // RandomAccessFile um an eine bestimmte Stelle zu schreiben
-                    RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
-                    
-                    // sichere die restliche Datei hinter der Baustelle um keine Inhalte durch überschreiben zu verlieren
-                    long fileSize = randomAccessFile.length();
-                    long remainingFileSize = fileSize - position;
-                    byte[] remainingBytes = new byte[(int) remainingFileSize];
-                    randomAccessFile.seek(position);
-                    randomAccessFile.readFully(remainingBytes);
-                    System.out.println(remainingBytes[0]);
-                    
-                    // schreibe den neuen Inhalt
-                    randomAccessFile.seek(position);
-                    randomAccessFile.writeBytes("127.0.0.1 " + textArea.getText());
-                    randomAccessFile.writeBytes(System.lineSeparator());
-                    
-                    // füge die restliche Datei wieder an
-                    // randomAccessFile.seek(randomAccessFile.length());
-                    randomAccessFile.write(remainingBytes);
-                    randomAccessFile.close();
-                    
-                    putToSave(textArea.getText());
-                    JOptionPane.showMessageDialog(this, "Text erfolgreich gespeichert.");
-                    textArea.setText("");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.err.println("Fehler beim Schreiben an eine bestimmte Stelle in der Datei.");
-                }
-        	}
-        }
-    }
+		// Additional Button: nimmt eine Zelle im Gitter ein
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridwidth = 1; // Eine Zelle in der Breite
+		gbc.gridheight = 1; // Eine Zelle in der Höhe
+		gbc.fill = GridBagConstraints.BOTH; // Füllt horizontal
+		gbc.weightx = 1.0; // Keine Gewichtung in horizontaler Richtung
+		gbc.weighty = 1.0; // Keine Gewichtung in vertikaler Richtung
+		getContentPane().add(manageButton, gbc);
 
-    public void putToSave(String text) {
-    	try {
-            // Erstellt eine BufferedWriter, um ans Ende der Datei zu schreiben
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filePathSave, true));
-            writer.write(text);
-            writer.newLine();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Fehler beim Speichern der Datei.");
-        }
-    }
-    
-    public String getFile(String fp) {
-    	try {
-            // Liest den gesamten Inhalt der Datei als String
-            Path path = Paths.get(fp);
-            byte[] fileBytes = Files.readAllBytes(path);
-            String fileContent = new String(fileBytes);
+		// Additional Button: nimmt eine Zelle im Gitter ein
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.gridwidth = 1; // Eine Zelle in der Breite
+		gbc.gridheight = 1; // Eine Zelle in der Höhe
+		gbc.fill = GridBagConstraints.BOTH; // Füllt horizontal
+		gbc.weightx = 1.0; // Keine Gewichtung in horizontaler Richtung
+		gbc.weighty = 1.0; // Keine Gewichtung in vertikaler Richtung
+		getContentPane().add(backButton, gbc);
 
-            // Verarbeitet den String (hier wird er einfach ausgegeben)
-            System.out.println("Inhalt der Datei:");
-            System.out.println(fileContent);
-            return fileContent;
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Fehler beim Lesen der Datei.");
-        }
-    	return "";
-    }
-    
-    public void checkForBackup() {
-    	
-    }
-    
-    public static void main(String[] args) {
-        // Erstellt eine Instanz der GUI und zeigt sie an
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new websiteBlocker().setVisible(true);
-            }
-        });
-    }
+		checkForBackup();
+	}
+
+	// Wenn der Benutzer auf "Speichern" klickt
+	private void speichern() {
+		// Zeigt einen Dialog zum Speichern an
+		// JFileChooser fileChooser = new JFileChooser();
+		// int result = fileChooser.showSaveDialog(this);
+
+		boolean positionFound = false;
+		int successfulWrites = 0;
+		long position = 0;
+		String searchWord = "#Website-Blacklist";
+
+		String regex = "^www\\.[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(\\n)?$";
+		Pattern pattern = Pattern.compile(regex);
+
+		String text = textArea.getText();
+		String[] lines = text.split("\\n");
+		for (String line : lines) {
+			line = line.replaceAll("\\n$", "");
+			Matcher matcher = pattern.matcher(line);
+			if (!matcher.matches() || line.length() < 7) {
+				//JOptionPane.showMessageDialog(this, "Webseite bitte im Format www.webseite.domain angeben.");
+			} else {
+				try {
+					// Öffnet die Datei zum Lesen
+					RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
+
+					String searchLine;
+					int lineNumber = 0;
+
+					// Durchsucht jede Zeile nach dem gesuchten Wort
+					while ((searchLine = randomAccessFile.readLine()) != null) {
+						lineNumber++;
+
+						// Überprüft, ob das gesuchte Wort in der Zeile enthalten ist
+						if (searchLine.contains(searchWord)) {
+							// System.out.println("Das Wort '" + searchWord + "' wurde in Zeile " +
+							// lineNumber + " gefunden.");
+							// Hier können Sie weitere Aktionen durchführen, z.B. die Position des Wortes in
+							// der Zeile anzeigen
+							position = randomAccessFile.getFilePointer();
+							positionFound = true;
+							// System.out.println("Position des Wortes: " + position);
+						}
+					}
+					// Schließt den BufferedReader
+					randomAccessFile.close();
+
+					if (!positionFound) {
+						BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+						writer.write("\n" + searchWord);
+						writer.newLine();
+						writer.close();
+						positionFound = true;
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.err.println("Fehler beim Lesen der Datei.");
+				}
+
+				if (filePath != null && positionFound) {
+					if (position == 0) {
+						try {
+							// Erstellt eine BufferedWriter, um ans Ende der Datei zu schreiben
+							BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+							writer.write("127.0.0.1 " + line);
+							writer.newLine();
+							writer.close();
+							
+							putToSave(line);
+							successfulWrites++;
+							textArea.setText("");
+						} catch (IOException e) {
+							e.printStackTrace();
+							JOptionPane.showMessageDialog(this, "Fehler beim Speichern der Datei.");
+						}
+					} else {
+						try {
+							// RandomAccessFile um an eine bestimmte Stelle zu schreiben
+							RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
+
+							// sichere die restliche Datei hinter der Baustelle um keine Inhalte durch
+							// überschreiben zu verlieren
+							long fileSize = randomAccessFile.length();
+							long remainingFileSize = fileSize - position;
+							byte[] remainingBytes = new byte[(int) remainingFileSize];
+							randomAccessFile.seek(position);
+							randomAccessFile.readFully(remainingBytes);
+							try {
+								//System.out.println(remainingBytes[0]);
+							} catch (IndexOutOfBoundsException e) {
+								e.printStackTrace();
+								System.err.println("Leere restliche Datei nach #Website-Blocker.");
+							}
+
+							// schreibe den neuen Inhalt
+							randomAccessFile.seek(position);
+							randomAccessFile.writeBytes("127.0.0.1 " + line);
+							randomAccessFile.writeBytes(System.lineSeparator());
+
+							// füge die restliche Datei wieder an
+							// randomAccessFile.seek(randomAccessFile.length());
+							randomAccessFile.write(remainingBytes);
+							randomAccessFile.close();
+
+							putToSave(line);
+							successfulWrites++;
+							textArea.setText("");
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.err.println("Fehler beim Schreiben an eine bestimmte Stelle in der Datei.");
+						}
+					}
+				}
+			}
+		}
+		if(successfulWrites == 1) {
+			JOptionPane.showMessageDialog(this, "Webseite wurde erfolgreich gespeichert.");
+		} 
+		else if(successfulWrites > 1) {
+			JOptionPane.showMessageDialog(this, successfulWrites + " Webseiten wurden erfolgreich gespeichert.");
+		}
+	}
+
+	public void putToSave(String text) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filePathSave, true));
+			//einfacher Test ob wirklich eine valide Webseite übergeben wurde
+			if(text.length() > 7) {
+				writer.write(text.replaceAll("\\n$", ""));
+				writer.newLine();
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Fehler beim Speichern der Datei.");
+		}
+	}
+
+	public String getFile(String fp) {
+		try {
+			// Liest den gesamten Inhalt der Datei als String
+			Path path = Paths.get(fp);
+			byte[] fileBytes = Files.readAllBytes(path);
+			String fileContent = new String(fileBytes);
+
+			// Verarbeitet den String (hier wird er einfach ausgegeben)
+			System.out.println("Inhalt der Datei:");
+			System.out.println(fileContent);
+			return fileContent;
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Fehler beim Lesen der Datei.");
+		}
+		return "";
+	}
+
+	public void checkForBackup() {
+
+	}
+
+	public static void main(String[] args) {
+		// Erstellt eine Instanz der GUI und zeigt sie an
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new websiteBlocker().setVisible(true);
+			}
+		});
+	}
 }
